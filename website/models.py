4651e6db980a57cd187f4148d2515f1dc00aab23
Horst Schneider <h.schneider@hs-mannheim.de>
2017-04-05 13:22:08 +0200

    Fix static files and add student model

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ class Thesis(models.Model):
     title = models.CharField(max_length=200)
     supervisor = models.ForeignKey('Supervisor', on_delete=models.CASCADE)
     assessor = models.ForeignKey('Assessor', on_delete=models.CASCADE)
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
 
     def __str__(self):
         return "{0}".format(self.title)
@@ -32,3 +33,14 @@ class Assessor(models.Model):
                                       self.email)
 
     __repr__ = __str__
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    id = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return "{0} {1} ({2})".format(self.first_name,
+                                      self.last_name,
+                                      self.id)
--- website/test/test_model_thesis.py
+++ website/test/test_model_thesis.py
@@ -10,36 +10,47 @@ setup_test_environment()
 class ThesisModelTests(TestCase):
 
     def test_create_model(self):
+        student = Student(first_name="Eva", last_name="Maier", id=123456)
         assessor = Assessor(first_name="Peter", last_name="Müller")
         supervisor = Supervisor(first_name="Thomas",
                                 last_name="Smits", id="t.smits")
 
         assessor.save()
         supervisor.save()
+        student.save()
 
         title = "Einsatz eines Flux-Kompensators für Zeitreisen" \
                 " mit einer maximalen Höchstgeschwindigkeit von WARP 7"
 
-        thesis = Thesis(assessor=assessor, supervisor=supervisor, title=title)
+        thesis = Thesis(student=student,
+                        assessor=assessor,
+                        supervisor=supervisor,
+                        title=title)
         thesis.save()
 
         self.assertEqual(1, Thesis.objects.count())
         self.assertEqual(supervisor, Thesis.objects.first().supervisor)
         self.assertEqual(assessor, Thesis.objects.first().assessor)
+        self.assertEqual(student, Thesis.objects.first().student)
         self.assertEqual(title, Thesis.objects.first().title)
 
     def test_cascading_delete_assessor(self):
+        student = Student(first_name="Eva", last_name="Maier", id=123456)
         assessor = Assessor(first_name="Peter", last_name="Müller")
         supervisor = Supervisor(first_name="Thomas",
                                 last_name="Smits", id="t.smits")
 
         assessor.save()
         supervisor.save()
+        student.save()
 
         title = "Einsatz eines Flux-Kompensators für Zeitreisen" \
                 " mit einer maximalen Höchstgeschwindigkeit von WARP 7"
 
-        thesis = Thesis(assessor=assessor, supervisor=supervisor, title=title)
+        thesis = Thesis(assessor=assessor,
+                        supervisor=supervisor,
+                        student=student,
+                        title=title)
         thesis.save()
 
         self.assertEqual(1, Thesis.objects.count())
@@ -49,17 +60,22 @@ class ThesisModelTests(TestCase):
         self.assertEqual(0, Thesis.objects.count())
 
     def test_cascading_delete_supervisor(self):
+        student = Student(first_name="Eva", last_name="Maier", id=123456)
         assessor = Assessor(first_name="Peter", last_name="Müller")
         supervisor = Supervisor(first_name="Thomas",
                                 last_name="Smits", id="t.smits")
 
         assessor.save()
         supervisor.save()
+        student.save()
 
         title = "Einsatz eines Flux-Kompensators für Zeitreisen" \
                 " mit einer maximalen Höchstgeschwindigkeit von WARP 7"
 
-        thesis = Thesis(assessor=assessor, supervisor=supervisor, title=title)
+        thesis = Thesis(student=student,
+                        assessor=assessor,
+                        supervisor=supervisor,
+                        title=title)
         thesis.save()
 
         self.assertEqual(1, Thesis.objects.count())
