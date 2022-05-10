67391c574900ff94c190cd8e0b66f4a4b36b2d8d
Linus Kanstein <86070097+HXG06@users.noreply.github.com>
2022-05-10 15:26:40 +0200

    urls.py anpassung an django4
    'url' ersetzt durch 'path' oder 're_path' (regex_path')
    login- und logout-views ersetzt

--- website/urls.py
+++ website/urls.py
@@ -1,34 +1,34 @@
-from django.conf.urls import url
-from django.contrib.auth.views import login, logout
+from django.urls import path, re_path
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
 from . import views
 from thesispool.pdf import *
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^accounts/login/$', login, {'template_name': 'website/login.html'},
-        name='login'),
-    url(r'^accounts/logout/$', logout, {'next_page': 'login'}, name='logout'),
-    url(r'^overview/$', views.overview, name='overview'),
-    url(r'^find-student/$', views.find_student, name='find_student'),
-    url(r'^create/(?P<student_id>\d+)$',
+    path('', views.index, name='index'),
+    path('accounts/login/', views.ThesispoolLoginView.as_view(),
+         name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('overview/', views.overview, name='overview'),
+    path('find-student/', views.find_student, name='find_student'),
+    re_path(r'create/(?P<student_id>\d+)',
         login_required(views.CreateThesis.as_view()), name='create'),
-    url(r'^download/application/(?P<key>[0-9a-f\-]+)$',
+    re_path(r'download/application/(?P<key>[0-9a-f\-]+)',
         login_required(views.PdfView.as_view()),
         {"type": ApplicationPDF},
         name='application_pdf'),
-    url(r'^download/prolongation/(?P<key>[0-9a-f\-]+)$',
+    re_path(r'download/prolongation/(?P<key>[0-9a-f\-]+)',
         login_required(views.PdfView.as_view()),
         {"type": ProlongationPDF},
         name='prolongation_pdf'),
-    url(r'^download/grading/(?P<key>[0-9a-f\-]+)$',
+    re_path(r'download/grading/(?P<key>[0-9a-f\-]+)',
         login_required(views.PdfView.as_view()),
         {"type": GradingPDF},
         name='grading_pdf'),
-    url(r'^prolong/(?P<key>[0-9a-f\-]+)$', views.prolong, name="prolong"),
-    url(r'^grade/(?P<key>[0-9a-f\-]+)$', views.grade, name="grade"),
-    url(r'^handin/(?P<key>[0-9a-f\-]+)$', views.handin, name="handin"),
-    url(r'^change/(?P<key>[0-9a-f\-]+)$',
+    re_path(r'prolong/(?P<key>[0-9a-f\-]+)', views.prolong, name="prolong"),
+    re_path(r'grade/(?P<key>[0-9a-f\-]+)', views.grade, name="grade"),
+    re_path(r'handin/(?P<key>[0-9a-f\-]+)', views.handin, name="handin"),
+    re_path(r'change/(?P<key>[0-9a-f\-]+)',
         login_required(views.ChangeView.as_view()), name="change"),
 ]
