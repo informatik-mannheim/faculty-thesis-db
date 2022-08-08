a4367665d31c5d3b4222857ff8b2feb14f666699
Linus Kanstein <86070097+LinusKanstein@users.noreply.github.com>
2022-08-08 11:43:32 +0200

    updated Test

--- website/test/test_view_delete.py
+++ website/test/test_view_delete.py
@@ -4,7 +4,7 @@ from datetime import date
 import uuid
 
 from website.models import Thesis
-from website.test.test import LoggedInTestCase, ThesisStub
+from website.test.test import LoggedInTestCase
 
 
 class ViewChangeTests(LoggedInTestCase):
@@ -16,11 +16,12 @@ class ViewChangeTests(LoggedInTestCase):
         self.assertEqual(404, response_get.status_code)
         self.assertEqual(404, response_post.status_code)
 
-    def test_delete_thesis(self):
+    def test_thesis_found(self):
         thesis = Thesis(student=self.student,
                         assessor=self.assessor,
                         supervisor=self.supervisor,
                         title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
                         begin_date=date(2018, 1, 30),
                         due_date=date(2018, 3, 30),
                         status=Thesis.APPLIED,
@@ -28,89 +29,90 @@ class ViewChangeTests(LoggedInTestCase):
 
         thesis.save()
 
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        response = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
+        self.assertEqual(200, response.status_code)
 
-        response_get = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
-        self.assertEqual(200, response_get.status_code)
+    def test_delete_thesis(self):
+        thesis = Thesis(student=self.student,
+                        assessor=self.assessor,
+                        supervisor=self.supervisor,
+                        title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
+                        begin_date=date(2018, 1, 30),
+                        due_date=date(2018, 3, 30),
+                        status=Thesis.APPLIED,
+                        restriction_note=False)
 
-        response_post = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
+        thesis.save()
 
-        self.assertEqual(302, response_post.status_code)
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), None)
+        response = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
+
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(Thesis.objects.first(), None)
 
     def test_delete_graded_thesis(self):
         thesis = Thesis(student=self.student,
                         assessor=self.assessor,
                         supervisor=self.supervisor,
                         title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
                         begin_date=date(2018, 1, 30),
                         due_date=date(2018, 3, 30),
+                        examination_date=date(2018, 4, 30),
                         status=Thesis.GRADED,
                         restriction_note=False)
 
         thesis.save()
 
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        response = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
 
-        response_get = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
-        self.assertEqual(302, response_get.status_code)
-
-        response_post = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
-
-        self.assertEqual(302, response_post.status_code)
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(Thesis.objects.first(), thesis)
 
     def test_delete_handed_in_thesis(self):
         thesis = Thesis(student=self.student,
                         assessor=self.assessor,
                         supervisor=self.supervisor,
                         title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
                         begin_date=date(2018, 1, 30),
                         due_date=date(2018, 3, 30),
+                        handed_in_date=date(2018, 3, 30),
                         status=Thesis.HANDED_IN,
-                        handed_in_date=date(2018, 2, 28),
                         restriction_note=False)
 
         thesis.save()
 
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        response = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
 
-        response_get = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
-        self.assertEqual(302, response_get.status_code)
-
-        response_post = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
-
-        self.assertEqual(302, response_post.status_code)
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(Thesis.objects.first(), thesis)
 
     def test_delete_prolonged_thesis(self):
         thesis = Thesis(student=self.student,
                         assessor=self.assessor,
                         supervisor=self.supervisor,
                         title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
                         begin_date=date(2018, 1, 30),
                         due_date=date(2018, 3, 30),
+                        prolongation_date=date(2018, 4, 30),
                         status=Thesis.PROLONGED,
-                        prolongation_date=date(2018, 2, 28),
                         restriction_note=False)
 
         thesis.save()
 
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        response = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
 
-        response_get = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
-        self.assertEqual(302, response_get.status_code)
-
-        response_post = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
-
-        self.assertEqual(302, response_post.status_code)
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(Thesis.objects.first(), thesis)
 
     def test_delete_approved_thesis(self):
         thesis = Thesis(student=self.student,
                         assessor=self.assessor,
                         supervisor=self.supervisor,
                         title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
                         begin_date=date(2018, 1, 30),
                         due_date=date(2018, 3, 30),
                         status=Thesis.APPLIED,
@@ -119,21 +121,17 @@ class ViewChangeTests(LoggedInTestCase):
 
         thesis.save()
 
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        response = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
 
-        response_get = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
-        self.assertEqual(302, response_get.status_code)
-
-        response_post = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
-
-        self.assertEqual(302, response_post.status_code)
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(Thesis.objects.first(), thesis)
 
     def test_delete_rejected_thesis(self):
         thesis = Thesis(student=self.student,
                         assessor=self.assessor,
                         supervisor=self.supervisor,
                         title="Eine einzelne Thesis",
+                        thesis_program=self.student.program,
                         begin_date=date(2018, 1, 30),
                         due_date=date(2018, 3, 30),
                         status=Thesis.APPLIED,
@@ -142,12 +140,7 @@ class ViewChangeTests(LoggedInTestCase):
 
         thesis.save()
 
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        response = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
 
-        response_get = self.client.get(reverse('delete', args=[thesis.surrogate_key]))
-        self.assertEqual(302, response_get.status_code)
-
-        response_post = self.client.post(reverse('delete', args=[thesis.surrogate_key]))
-
-        self.assertEqual(302, response_post.status_code)
-        self.assertEqual(Thesis.objects.filter(id=thesis.id).first(), thesis)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(Thesis.objects.first(), thesis)
